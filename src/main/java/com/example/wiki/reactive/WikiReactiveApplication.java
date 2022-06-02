c35893659555dacec5c148cfd299e7510404222e
AndriiBosyk <andriybosik2002@ukr.net>
2022-06-02 22:06:53 +0300

    Initial project

---
+++ src/main/java/com/example/wiki/reactive/WikiReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.wiki.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiReactiveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WikiReactiveApplication.class, args);
+  }
+
+}
