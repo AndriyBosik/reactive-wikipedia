b04d7f53f5ef757ad26e3986e27b968ec7483a37
AndriiBosyk <andriybosik2002@ukr.net>
2022-06-08 19:59:33 +0300

    Minor fix

--- src/main/java/com/example/wiki/reactive/meta/Endpoint.java
+++ src/main/java/com/example/wiki/reactive/meta/Endpoint.java
@@ -5,8 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Endpoint {
   private static final String API_PREFIX = "/api";
-  private static final String V1_VERSION = "/v1";
-  private static final String API_V1 = API_PREFIX + V1_VERSION;
 
-  public static final String RECENT_CHANGES = API_V1 + "/recent-changes";
+  public static final String RECENT_CHANGES = API_PREFIX + "/recent-changes";
 }
